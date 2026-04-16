e5ce1681e4cf0bbd8b9e2ec135f89f6a2925854d
martin <goudingcheng@gmail.com>
2026-04-16 22:09:45 +0800

    feature:yugabytedb db dialect provider

---
+++ src/main/java/com/github/paohaijiao/provider/impl/JQuickYugabyteDBDialectProvider.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+package com.github.paohaijiao.provider.impl;
+
+import com.github.paohaijiao.dialect.JQuickSQLDialect;
+import com.github.paohaijiao.dialect.impl.JQuickYugabyteDBDialect;
+import com.github.paohaijiao.provider.JQuickSQLDialectProvider;
+
+/**
+ * YugabyteDB 方言提供者
+ * YugabyteDB 兼容 PostgreSQL 协议
+ *
+ * @author Martin
+ * @version 1.0.0
+ * @since 2026/4/16
+ */
+public class JQuickYugabyteDBDialectProvider implements JQuickSQLDialectProvider {
+
+    @Override
+    public String getDatabaseType() {
+        return "yugabytedb";
+    }
+
+    @Override
+    public JQuickSQLDialect createDialect() {
+        return new JQuickYugabyteDBDialect();
+    }
+
+    @Override
+    public boolean supports(String jdbcUrl) {
+        if (jdbcUrl == null) {
+            return false;
+        }
+        if (jdbcUrl.startsWith("jdbc:postgresql://")) {
+            String lowerUrl = jdbcUrl.toLowerCase();
+            return lowerUrl.contains("yugabyte") || lowerUrl.contains("yugabytedb") || lowerUrl.contains(":5433");
+        }
+        if (jdbcUrl.startsWith("jdbc:yugabytedb:")) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getPriority() {
+        return 150;
+    }
+}
