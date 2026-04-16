c0f142588b3ea5efe950c6a8b6949a88997b0781
martin <goudingcheng@gmail.com>
2026-04-16 21:57:45 +0800

    feature:snowflake db dialect provider

---
+++ src/main/java/com/github/paohaijiao/provider/impl/JQuickSnowflakeDialectProvider.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+package com.github.paohaijiao.provider.impl;
+
+/**
+ * packageName com.github.paohaijiao.provider.impl
+ *
+ * @author Martin
+ * @version 1.0.0
+ * @since 2026/4/16
+ */
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+
+import com.github.paohaijiao.dialect.JQuickSQLDialect;
+import com.github.paohaijiao.dialect.impl.JQuickSnowflakeDialect;
+import com.github.paohaijiao.provider.JQuickSQLDialectProvider;
+
+/**
+ * Snowflake 方言提供者
+ *
+ * @author Martin
+ * @version 1.0.0
+ * @since 2026/4/16
+ */
+public class JQuickSnowflakeDialectProvider implements JQuickSQLDialectProvider {
+
+    @Override
+    public String getDatabaseType() {
+        return "snowflake";
+    }
+
+    @Override
+    public JQuickSQLDialect createDialect() {
+        return new JQuickSnowflakeDialect();
+    }
+
+    @Override
+    public boolean supports(String jdbcUrl) {
+        if (jdbcUrl == null) {
+            return false;
+        }
+        return jdbcUrl.startsWith("jdbc:snowflake:");
+    }
+
+    @Override
+    public int getPriority() {
+        return 200;
+    }
+}
