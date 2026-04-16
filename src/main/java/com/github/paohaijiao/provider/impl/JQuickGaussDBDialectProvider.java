2f255e5f10bce4c6c118b6b6f58d55656e8d2c7d
martin <goudingcheng@gmail.com>
2026-04-16 21:38:09 +0800

    feature:gauss db dialect provider

---
+++ src/main/java/com/github/paohaijiao/provider/impl/JQuickGaussDBDialectProvider.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+package com.github.paohaijiao.provider.impl;
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+
+import com.github.paohaijiao.dialect.JQuickSQLDialect;
+import com.github.paohaijiao.dialect.impl.JQuickGaussDBDialect;
+import com.github.paohaijiao.provider.JQuickSQLDialectProvider;
+
+/**
+ * GaussDB 方言提供者
+ * GaussDB 兼容 PostgreSQL 协议
+ *
+ * @author Martin
+ * @version 1.0.0
+ * @since 2026/4/15
+ */
+public class JQuickGaussDBDialectProvider implements JQuickSQLDialectProvider {
+
+    @Override
+    public String getDatabaseType() {
+        return "gaussdb";
+    }
+
+    @Override
+    public JQuickSQLDialect createDialect() {
+        return new JQuickGaussDBDialect();
+    }
+
+    @Override
+    public boolean supports(String jdbcUrl) {
+        if (jdbcUrl == null) {
+            return false;
+        }
+
+        if (jdbcUrl.startsWith("jdbc:postgresql://")) {
+            String lowerUrl = jdbcUrl.toLowerCase();
+            return lowerUrl.contains("gaussdb") || lowerUrl.contains("gauss");
+        }
+        if (jdbcUrl.startsWith("jdbc:gaussdb:")) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getPriority() {
+        return 150;
+    }
+}
